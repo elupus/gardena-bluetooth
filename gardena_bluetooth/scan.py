5f3681ce0fd21b2f85216b2cdbeefffcc2ed1666
Joakim Plate <elupus@ecce.se>
2026-04-01 23:09:42 +0200

    fix: only use features of BleakScanner available in habluetooth

--- gardena_bluetooth/scan.py
+++ gardena_bluetooth/scan.py
@@ -1,8 +1,9 @@
 import asyncio
+import contextlib
 import logging
 from contextlib import suppress
 
-from bleak import BleakScanner, BaseBleakScanner
+from bleak import BleakScanner, BaseBleakScanner, AdvertisementData, BLEDevice
 
 from .parse import ManufacturerData
 from .const import ScanService
@@ -11,6 +12,36 @@ LOGGER = logging.getLogger(__name__)
 
 DEFAULT_MANUFACTURER_DATA_PRODUCT_TYPE_FIELDS = {"group", "model", "variant"}
 DEFAULT_MANUFACTURER_DATA_TIMEOUT = 5.0
+
+
+@contextlib.asynccontextmanager
+async def advertisement_queue(backend: type[BaseBleakScanner] | None = None):
+    """
+    Context manager for BleakScanner
+
+    Some implementations do not support async context management protocol,
+    this include older versions of bleak, as well as the wrapper used in
+    home assistant (see https://github.com/Bluetooth-Devices/habluetooth/issues/386).
+
+    We can't use the async iterator of the scanner, since some
+    implementations (like the one in home assistant) does not
+    support it. See https://github.com/Bluetooth-Devices/habluetooth/issues/380
+    """
+
+    queue = asyncio.Queue[tuple[BLEDevice, AdvertisementData]]()
+
+    def _callback(device, advertisement):
+        queue.put_nowait((device, advertisement))
+
+    scanner = BleakScanner(
+        backend=backend, service_uuids=[ScanService], detection_callback=_callback
+    )
+
+    await scanner.start()
+    try:
+        yield queue
+    finally:
+        await scanner.stop()
 
 
 async def async_get_manufacturer_data(
@@ -28,10 +59,9 @@ async def async_get_manufacturer_data(
         return data
 
     with suppress(TimeoutError):
-        async with asyncio.timeout(timeout), BleakScanner(
-            backend=backend, service_uuids=[ScanService]
-        ) as scanner:
-            async for device, advertisement in scanner.advertisement_data():
+        async with advertisement_queue(backend) as queue, asyncio.timeout(timeout):
+            while True:
+                device, advertisement = await queue.get()
                 if device.address not in addresses:
                     continue
                 mfg_data = data[device.address]
